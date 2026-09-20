c935f06e4088e4f92b33cf5b5650db29a3d7a33f
MunkiButtt <138344365+MunkiButtt@users.noreply.github.com>
2026-09-20 09:27:55 -0400

    Update GrabenUndReinigen.lua

--- scripts/GrabenUndReinigen.lua
+++ scripts/GrabenUndReinigen.lua
@@ -1454,7 +1454,7 @@ end
 
 local function InventoryFull()
     local inv = GetInventory()
-    return inv ~= nil and #inv >= 50 or false
+    return inv ~= nil and #inv >= 1 or false
 end
 
 function DigLoop()
